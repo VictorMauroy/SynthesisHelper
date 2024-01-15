d638ffd07fb84d246645987ee08fcd5fb2e8d814
VictorMauroy <mauroy.victor@gmail.com>
2024-01-15 14:48:31 +0100

    feat: edit the monster model with more properties and fix similar column names

--- SynthesisAPI/Models/Monster.cs
+++ SynthesisAPI/Models/Monster.cs
@@ -5,31 +5,55 @@ namespace SynthesisAPI.Models;
 
 public class Monster {
     
-    [Key]
-    [Column("id")]
+    [Column("id"), Key]
     public Guid MonsterId { get; set; } = Guid.NewGuid();
 
 
+    //Informations
     [Column("name"), MaxLength(100)]
     public required string Name { get; set; }
     
-    [Column("name"), MaxLength(20)]
-    public required string GameID { get; set; }
+    [Column("gameid"), MaxLength(20)]
+    public string? GameID { get; set; }
     
-    [Column("name"), MaxLength(20)]
+    [Column("family"), MaxLength(20)]
     public required string Family { get; set; }
     
-    [Column("name")]
+    [Column("rank")]
     public required char Rank { get; set; }
 
+    [Column("details"), MaxLength(500)]
+    public required string Details { get; set; }
+
+    [Column("statistics")]
+    public int[] Statistics { get; set; } = new int[5];
+
+
+    // Management
+    [Column("reservation_date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime CreationDate { get; set; }
+
+    [Column("isactive")]
     public bool IsActive { get; set; } = true;
     
+    
+    // Relationship
+    [Column("combinations")]
+    public List<List<Monster>>? Combinations { get; set; }
+
+    [Column("special_combinations")]
+    public List<List<Monster>>? SpecialCombinations { get; set; }
 
     /*
-    Location with season and/or obtention ways (events, quests)
-    Statistics (Ex: Strength, wisdom; from 0 to 5)
-    Combinations (Classic)
-    Combinations (Special)
-    Creation date
+    Location with season
+    How to get (events, quests)
+    English name
+    Japanese name
+    Talents
+    Traits
+    Skills
+    Weaknesses, Resistances
     */
 }
