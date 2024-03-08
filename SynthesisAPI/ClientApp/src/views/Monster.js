7a508d55e4826bc5926b56285a291136349864a1
VictorMauroy <mauroy.victor@gmail.com>
2024-03-08 18:39:53 +0100

    feat: improve the display of a single monster unit with more details

--- SynthesisAPI/ClientApp/src/views/Monster.js
+++ SynthesisAPI/ClientApp/src/views/Monster.js
@@ -7,22 +7,29 @@ export default function Monster() {
     let { monsterId } = useParams();
     const [monster, setMonster] = useState(null);
 
-    console.log(monster);
-
     useEffect( () => {
-        fetch("http://localhost:5051/Monster/" + monsterId)
+        fetch(monsterApiUrl + "/Monster/" + monsterId)
         .then(res => res.json())
         .then((data) => {
             setMonster(data)
         });
     }, []);
 
-    return(
-        <>
-            <p> 
-                There is a monster here! <br />
-                Its name is {monster && monster.name}
-            </p>
-        </>
-    );
+    if(monster != null) {
+        return(
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Name: {monster.name}</h5>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">Rank: {monster.rank}</h6>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">Game ID: {monster.gameID}</h6>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">Family: {monster.family}</h6>
+                    <p className="card-text">{monster.details}</p>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <p>Loading monster data...</p>
+        );
+    }
 }
